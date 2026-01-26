4a62980f0e37fd795fb8145bf2696fa627e75a45
Nischay Arora <nischayarora701@gmail.com>
2026-01-26 21:03:55 +0530

    Fix aliasing behavior demonstration in demonstrate_alias_trap function

--- project/python_palette.py
+++ project/python_palette.py
@@ -384,10 +384,10 @@ def demonstrate_alias_trap():
     l = [1,2,3]
     a = l
     l = l + [4]
-    concat_breaks_alias = l is a
+    concat_breaks_alias = l is not a
 
     l = [1,2,3]
     a = l
     l += [4]
-    plus_equals_keeps_alias = l is a
+    plus_equals_keeps_alias = l is not a
     return (concat_breaks_alias, plus_equals_keeps_alias)
